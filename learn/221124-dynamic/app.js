51aa915325bff1549c2701de790fb2925f3f1ebb
namhyong <namhyong2@gmail.com>
2022-11-25 18:46:39 +0900

    feat:20221125

--- learn/221124-dynamic/app.js
+++ learn/221124-dynamic/app.js
@@ -17,7 +17,7 @@
             res.render("post");
         })
 
-        app.post("/post2",function(req,res){
+        app.post("/post3",function(req,res){
             // console.log(req.body);   
 
             if (req.body.id =="namhyong" &&
@@ -27,9 +27,92 @@
                 res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
             });    
             
-            
-            
+
+            app.get("/fetch",function(req,res){
+                res.render("fetch");
+            })
     
+            app.post("/fetchpost",function(req,res){
+                // console.log(req.body);   
+    
+                if (req.body.id =="namhyong" &&
+                req.body.pw =="1234"){
+                    res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+                else {
+                    res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+                }); 
+
+            
+            app.get("/fetchget",function(req,res){
+                // console.log(req.body);   
+        
+                if (req.query.id =="namhyong" &&
+                req.query.pw =="1234"){
+                    res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+                else {
+                    res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+                });     
+                
+                
+
+
+
+                app.get("/axios",function(req,res){
+                    res.render("axios");
+                })
+        
+                app.post("/axiospost",function(req,res){
+                    // console.log(req.body);   
+        
+                    if (req.body.id =="namhyong" &&
+                    req.body.pw =="1234"){
+                        res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+                    else {
+                        res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+                    }); 
+    
+                
+                app.get("/axiosget",function(req,res){
+                    // console.log(req.body);   
+            
+                    if (req.query.id =="namhyong" &&
+                    req.query.pw =="1234"){
+                        res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+                    else {
+                        res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+                    });        
+                
+            
+
+                    app.get("/ajax",function(req,res){
+                        res.render("ajax");
+                    })
+            
+                    app.post("/ajaxpost",function(req,res){
+                        // console.log(req.body);   
+            
+                        if (req.body.id =="namhyong" &&
+                        req.body.pw =="1234"){
+                            res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+                        else {
+                            res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+                        }); 
+        
+                    
+                    app.get("/ajaxget",function(req,res){
+                        // console.log(req.body);   
+                
+                        if (req.query.id =="namhyong" &&
+                        req.query.pw =="1234"){
+                            res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+                        else {
+                            res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+                        });     
+                        
+                            
+
+
+
         
 
             // axios 버전
