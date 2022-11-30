4d3d40211dce4e85c5c29a0e5aa48137e7bac03b
namhyong <namhyong2@gmail.com>
2022-11-30 21:44:30 +0900

    feat:20221130

--- learn/221129.mysql/index.js
+++ learn/221129.mysql/index.js
@@ -20,15 +20,34 @@ app.use(express.json())
 app.use("/static",express.static("static"))
 
 
-app.get('/',(req,res)=>{
-cnn.query('select * from user where ',(err,result)=>{   
-    if(err) throw err 
+app.get("/ajax",function(req,res){
+    res.render("ajax");
+})
 
-    console.log(result);
-    res.render("index",{rows:result});  //mysql데이터베이스에서 가져온 user테이블 데이터를(result) rows에담아 랜더한다.
-    })
-    
+app.post("/ajaxpost",function(req,res){
+    // console.log(req.body);   
+
+    if (req.body.id =="namhyong" &&
+    req.body.pw =="1234"){
+        res.send({msg:"<p style='color:blue'>로그인 성공</p>"});}
+    else {
+        res.send({msg:"<p style='color:red'>로그인 실패</p>"})};
+    }); 
+
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs")
 });
+app.post("/sqlpost",function(req,res){
+// cnn.query('select * from user where ',(err,result)=>{   
+    cnn.query('select * from member  ',(err,result)=>{
+        if(err) throw err 
+    
+        console.log(result);
+        res.render("index",{rows:result});  //mysql데이터베이스에서 가져온 user테이블 데이터를(result) rows에담아 랜더한다.
+        })
+    })
+        
 
 app.listen(port,()=>{
     console.log("open:", port);
